f91eada5c016308ca475a95275a28aab971d3abe
ramakrishna.mb <ramakrishna.mb@excelindia.com>
2019-11-22 17:45:40 +0530

    Add project files.

---
+++ DotnetCoreWebApp/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DotnetCoreWebApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Note: AddMvc also have built in AddMvcCore , so intitalzing AddMvc services support both MVC conepts along with the core concepts --RK
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();// this is needed because, if request will made for css,js then this middle ware needs be above mvc middlewear --RK
+            app.UseMvcWithDefaultRoute();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
---
+++ DotnetCoreWebApp/controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCoreWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public JsonResult Index()
+        {
+
+            return Json(new { type = "this annomous object on fly", Id = 123 });
+        }
+    }
+}
